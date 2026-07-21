9baf229d806c6e377907237f3b79d818819f2c2e
Pooh <pooh250841111@gmail.com>
2026-07-21 13:59:06 +0700

    fix: update Google service configuration to include client ID, client secret, and redirect URI

--- config/services.php
+++ config/services.php
@@ -36,7 +36,10 @@ return [
     ],
 
     'google' => [
-        'geocode_key' => env('GOOGLE_GEOCODE_KEY'),
-    ],
+    'client_id' => env('GOOGLE_CLIENT_ID'),
+    'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+    'redirect' => env('GOOGLE_REDIRECT_URI'),
+    'geocode_key' => env('GOOGLE_GEOCODE_KEY'),
+],
 
 ];
